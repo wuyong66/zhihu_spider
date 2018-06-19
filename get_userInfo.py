bdd4421854258c40f35261979eb76e0ea9a673b2
wuyong <1062073083@qq.com>
2018-06-19 11:11:45 +0800

    update

--- get_userInfo.py
+++ get_userInfo.py
@@ -19,11 +19,11 @@ def get_userInfo(userID):
     account = ZhihuAccount()
     account.login(username=None, password=None, load_cookies=True)
     id = userID
-    user_url = 'https://www.zhihu.com/people/' + id + '/activities'
+    user_url = 'https://www.zhihu.com/people/' + id
     response = account.session.get(user_url, headers=login.HEADERS)  # 利用这个session可以维持一个会话，保持登录状态
     #print(response.content.decode('utf-8'))  #decode解码
     soup = BeautifulSoup(response.content, 'lxml')
-    print(soup.prettify())   #以标准格式显示html源码
+    #print(soup.prettify())   #以标准格式显示html源码
     name = soup.title.string.split()[0]  # 把标签以外的内容打印出来，使用split分开,选取第一个字符串
     print("name: %s" % name)
     ID = id
@@ -32,6 +32,8 @@ def get_userInfo(userID):
     print("university: %s" % university)
     major = soup.find(name = 'div', class_="ProfileHeader-infosDivider").find_next_sibling().string
     print("major: %s" % major)
+
+
 
 
 
